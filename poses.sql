675d293ee3f62bf115441ed50440d06782c92337
Shafyah <shafaih20@gmail.com>
2025-08-02 21:39:33 +0300

    Update poses.sql

--- poses.sql
+++ poses.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 02 أغسطس 2025 الساعة 20:22
--- إصدار الخادم: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `robot-arm`
---
-
--- --------------------------------------------------------
-
---
--- بنية الجدول `poses`
---
 
 CREATE TABLE `poses` (
   `id` int(11) NOT NULL,
@@ -37,34 +15,17 @@ CREATE TABLE `poses` (
   `motor6` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- إرجاع أو استيراد بيانات الجدول `poses`
---
+
 
 INSERT INTO `poses` (`id`, `motor1`, `motor2`, `motor3`, `motor4`, `motor5`, `motor6`) VALUES
 (24, 151, 13, 63, 98, 72, 111);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `poses`
---
 ALTER TABLE `poses`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `poses`
---
 ALTER TABLE `poses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
